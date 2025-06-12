724523f77a7527ff35d7fe8ced705fcd62fab267
Nate <142466177+NateCamChiao@users.noreply.github.com>
2025-06-12 02:33:40 -0700

    setDesiredState update to modules

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -7,9 +7,13 @@ import com.studica.frc.AHRS.NavXComType;
 import frc.robot.Constants;
 import frc.robot.subsystems.SwerveModule;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Swerve extends SubsystemBase{
@@ -26,26 +30,34 @@ public class Swerve extends SubsystemBase{
             new SwerveModule(BLModule.kSwerveConstants),
             new SwerveModule(BRModule.kSwerveConstants)
         };
+        //avoid but inverting error
+        //TODO put this in its own thread
+        Timer.delay(1.0);
+        alignModules();
         this.swerveDriveOdometry = new SwerveDriveOdometry(
             Constants.Swerve.kSwerveKinematics,
             this.getYaw(),
             getModulePositions()
         );
-
     }
     @Override
     public void periodic(){
-
+        this.swerveDriveOdometry.update(getYaw(), getModulePositions());
     }
 
-    public SwerveModulePosition[] getModulePositions(){
-        SwerveModulePosition[] modPositions = new SwerveModulePosition[4];
-        for(int i = 0; i < this.swerveModules.length; i++){
-            modPositions[i] = this.swerveModules[i].getModulePosition();
+    public void drive(Translation2d translation, double rotationRad, boolean isFieldRelative){
+        ChassisSpeeds chassisSpeeds;
+        if(isFieldRelative){
+            chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(translation.getX(), translation.getY(), rotationRad, getYaw());
         }
-        return modPositions;
+        else{
+            chassisSpeeds = new ChassisSpeeds(translation.getX(), translation.getY(), rotationRad);
+        }
+        SwerveModuleState[] moduleStates = kSwerveKinematics.toSwerveModuleStates(chassisSpeeds);
+        for(int i = 0; i < this.swerveModules.length; i++){
+            this.swerveModules[i].setDesiredState(moduleStates[i]);
+        }
     }
-
     //gyro
 
     public void zeroGyro(){
@@ -55,11 +67,17 @@ public class Swerve extends SubsystemBase{
     public Rotation2d getYaw(){
         return Rotation2d.fromDegrees(360 - m_gryo.getAngle());
     }
-
+    //sweve modules
     public void alignModules(){
         for(SwerveModule mod : this.swerveModules){
             mod.resetToAbsolute();
         }
     }
-
+    public SwerveModulePosition[] getModulePositions(){
+        SwerveModulePosition[] modPositions = new SwerveModulePosition[4];
+        for(int i = 0; i < this.swerveModules.length; i++){
+            modPositions[i] = this.swerveModules[i].getModulePosition();
+        }
+        return modPositions;
+    }
 }
--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -1,25 +1,54 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import frc.robot.Constants;
 
 public class SwerveModule {
     private TalonFX m_angleMotor, m_driveMotor;
-    private CANcoder m_angleEncoder;
+    private CANcoder m_encoder;
+    private DutyCycleOut m_driveDutyCycle;
+    private PositionVoltage m_anglePositionVoltage;
+
     public SwerveModule(Constants.SwerveConstants moduleIDConstants){
         m_angleMotor = new TalonFX(moduleIDConstants.kDriveMotorID());
         m_driveMotor = new TalonFX(moduleIDConstants.kAngleMotorID());
-        m_angleEncoder = new CANcoder(moduleIDConstants.kCancoderID());
-    }
+        m_encoder = new CANcoder(moduleIDConstants.kCancoderID());
 
+        m_angleMotor.getConfigurator().apply()
+    }
+    //sets integrated encoder to absolute position
+    //eliminates need to manually align swerves
     public void resetToAbsolute(){
-        
+        //difference between absolute angle and relative angle
+        double absoultePosition = m_encoder.getPosition().getValueAsDouble() - m_angleMotor.getPosition().getValueAsDouble();
+        this.m_angleMotor.setPosition(absoultePosition);
     }
 
+
+    public void setDesiredState(SwerveModuleState desiredState){
+        desiredState.optimize(getAngle());
+        setAngle(desiredState.angle);
+        //takes difference between angle setpoint and current angle
+        //then take cos of angle to limit speed when angle difference is large
+        setDriveSpeed(desiredState.angle.minus(getAngle()).getCos());
+    }
+
+    public void setAngle(Rotation2d angle){
+        m_angleMotor.setControl(m_anglePositionVoltage.withPosition(angle.getRotations()));
+    }
+
+    public void setDriveSpeed(double speedMetersPerSecond){
+        m_driveDutyCycle.Output = speedMetersPerSecond / Constants.Swerve.kMaxSpeedMetersPerSec;
+        m_driveMotor.setControl(m_driveDutyCycle);
+    }
+    //gets module positions for odometry
     public SwerveModulePosition getModulePosition(){
         return new SwerveModulePosition(
             getDriveDistMeters(),
@@ -32,6 +61,6 @@ public class SwerveModule {
     }
 
     public double getDriveDistMeters(){
-        return m_driveMotor.getPosition().getValueAsDouble() / Constants.Swerve.wheelCircumference;
+        return m_driveMotor.getPosition().getValueAsDouble() * Constants.Swerve.wheelCircumference;
     }
 }
